fa00468db8e7521fda520983d5d6532be194b319
Ricardo <ricardobozzi23@gmail.com>
2024-03-04 17:32:56 +0100

    Add border to project sqares

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -3,34 +3,34 @@ import "../stylesheets/projects.css";
 export default function Projects() {
   return (
     <div className="section projects-section">
-      <section className="projects__inner contained">
+      <section className="projects__inner">
         <h1 className="title projects__title title-light">
           mieiProgetti.<span className="projects__title-span">Selezionati</span>
         </h1>
         <div className="projects-grid">
           <article className="project hor project-light">
-            <h3 className="project__title">Title</h3>
-            <a href="#" className="project__btn">
+            <h3 className="project__title">Smartahol</h3>
+            {/* <a href="#" className="project__btn">
               View
-            </a>
+            </a> */}
           </article>
           <article className="project ver project-dark">
-            <h3 className="project__title">Title</h3>
-            <a href="#" className="project__btn">
+            <h3 className="project__title">The Coloured Box Game</h3>
+            {/* <a href="#" className="project__btn">
               View
-            </a>
+            </a> */}
           </article>
           <article className="project hor project-light">
-            <h3 className="project__title">Title</h3>
-            <a href="#" className="project__btn">
+            <h3 className="project__title">Pokemon Kanto Pokedex</h3>
+            {/* <a href="#" className="project__btn">
               View
-            </a>
+            </a> */}
           </article>
           <article className="project ver project-dark">
-            <h3 className="project__title">Title</h3>
-            <a href="#" className="project__btn">
+            <h3 className="project__title">Dark Souls Paper Rock Scissors</h3>
+            {/* <a href="#" className="project__btn">
               View
-            </a>
+            </a> */}
           </article>
         </div>
       </section>
