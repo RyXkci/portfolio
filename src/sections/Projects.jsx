ae43545730a9603dd7f9f90064ec319065352586
Ricardo <ricardobozzi23@gmail.com>
2024-03-04 19:42:21 +0100

    added hover effect to project squares

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -10,15 +10,20 @@ export default function Projects() {
         <div className="projects-grid">
           <article className="project hor project-light">
             <h3 className="project__title">Smartahol</h3>
-            {/* <a href="#" className="project__btn">
+            <div className="project__link-container">
+               <a href="#" className="project__btn">
               View
-            </a> */}
+            </a>
+            </div>
+            
           </article>
           <article className="project ver project-dark">
             <h3 className="project__title">The Coloured Box Game</h3>
-            {/* <a href="#" className="project__btn">
+            <div className="project__link-container">
+               <a href="#" className="project__btn">
               View
-            </a> */}
+            </a>
+            </div>
           </article>
           <article className="project hor project-light">
             <h3 className="project__title">Pokemon Kanto Pokedex</h3>
@@ -28,6 +33,12 @@ export default function Projects() {
           </article>
           <article className="project ver project-dark">
             <h3 className="project__title">Dark Souls Paper Rock Scissors</h3>
+            {/* <a href="#" className="project__btn">
+              View
+            </a> */}
+          </article>
+          <article className="project hor project-light">
+            <h3 className="project__title">FEM Calcolatrice Mancie</h3>
             {/* <a href="#" className="project__btn">
               View
             </a> */}
